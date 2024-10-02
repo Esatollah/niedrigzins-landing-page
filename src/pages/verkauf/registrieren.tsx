1aafa1ec3217664e6262a4201b457ebddb369110
izzy <g.esatollah@yahoo.com>
2024-10-02 23:07:22 +0200

    .

--- src/pages/verkauf/registrieren.tsx
+++ src/pages/verkauf/registrieren.tsx
@@ -23,9 +23,7 @@ export default function Page() {
         src="https://tally.so/widgets/embed.js"
         strategy="lazyOnload"
         onLoad={() => {
-          //@ts-expect-error
           if (window.Tally) {
-            //@ts-expect-error
             window.Tally.loadEmbeds();
           }
         }}
